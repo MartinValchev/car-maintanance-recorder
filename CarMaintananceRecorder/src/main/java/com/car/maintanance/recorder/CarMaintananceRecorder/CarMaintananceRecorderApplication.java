bbda2a97bebcaa1554f6373c7d826bca13e5b657
mvalchev <martinvalchev1@gmail.com>
2026-08-31 22:40:30 +0300

    initial project state

---
+++ CarMaintananceRecorder/src/main/java/com/car/maintanance/recorder/CarMaintananceRecorder/CarMaintananceRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.car.maintanance.recorder.CarMaintananceRecorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMaintananceRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMaintananceRecorderApplication.class, args);
+	}
+
+}
---
+++ CarMaintananceRecorder/src/test/java/com/car/maintanance/recorder/CarMaintananceRecorder/CarMaintananceRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.maintanance.recorder.CarMaintananceRecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMaintananceRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
