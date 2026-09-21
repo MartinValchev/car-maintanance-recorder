301a575b88e70f05fbfbe4cb971d73f41bb0c2b7
mvalchev <martinvalchev1@gmail.com>
2026-09-21 20:12:10 +0300

    fix settings.kts

--- CarMaintananceRecorder/.teamcity/settings.kts
+++ CarMaintananceRecorder/.teamcity/settings.kts
@@ -1,7 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import javax.net.ssl.SSLContext
 
 version = "2024.03"
 
@@ -9,19 +8,25 @@ project {
     buildType(Build)
 }
 
-object Build: BuildType({
-    name = "Build and test"
+object Build : BuildType({
+    name = "Build and Test"
+
     vcs {
         root(DslContext.settingsRoot)
     }
+
     steps {
         maven {
+            name = "Maven Build and Test"
             goals = "clean test"
-            pomLocation = "pom.xml"
+            pomLocation = "CarMaintananceRecorder/pom.xml"
+            jvmArgs = "-Xmx512m"
         }
     }
 
-    triggers = {
-        vcs {}
+    triggers {
+        vcs {
+            branchFilter = "+:*"
+        }
     }
 })
