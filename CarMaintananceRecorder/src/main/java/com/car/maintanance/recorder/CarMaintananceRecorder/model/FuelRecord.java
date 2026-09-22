44111372c0d26331031f152ef653e74c48db008c
mvalchev <martinvalchev1@gmail.com>
2026-09-22 19:59:42 +0300

    Revert "no database annotations"
    This reverts commit 8468e4d2440b004f003f776b6cb26b36fea40788.

--- CarMaintananceRecorder/src/main/java/com/car/maintanance/recorder/CarMaintananceRecorder/model/FuelRecord.java
+++ CarMaintananceRecorder/src/main/java/com/car/maintanance/recorder/CarMaintananceRecorder/model/FuelRecord.java
@@ -1,5 +1,6 @@
 package com.car.maintanance.recorder.CarMaintananceRecorder.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
--- CarMaintananceRecorder/src/main/java/com/car/maintanance/recorder/CarMaintananceRecorder/model/ServiceRecord.java
+++ CarMaintananceRecorder/src/main/java/com/car/maintanance/recorder/CarMaintananceRecorder/model/ServiceRecord.java
@@ -1,5 +1,6 @@
 package com.car.maintanance.recorder.CarMaintananceRecorder.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +10,8 @@ import java.util.UUID;
 @Setter
 //@Entity
 public class ServiceRecord extends AbstractRecord {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.UUID)
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String serviceType;
